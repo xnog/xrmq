63d48d953d6a4703a31004d81df1c3e899f7aee8
Felipe Nogueira Gaudio <xnogueira@gmail.com>
2023-03-19 21:52:18 +0100

    set json serializer defaults

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,9 +1,23 @@
+using System.Reflection;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using Api;
 using X;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+typeof(JsonSerializerOptions).GetRuntimeFields()
+    .Single(f => f.Name == "_defaultIgnoreCondition")
+    .SetValue(JsonSerializerOptions.Default, JsonIgnoreCondition.WhenWritingNull);
+typeof(JsonSerializerOptions).GetRuntimeFields()
+    .Single(f => f.Name == "_dictionaryKeyPolicy")
+    .SetValue(JsonSerializerOptions.Default, JsonNamingPolicy.CamelCase);
+typeof(JsonSerializerOptions).GetRuntimeFields()
+    .Single(f => f.Name == "_jsonPropertyNamingPolicy")
+    .SetValue(JsonSerializerOptions.Default, JsonNamingPolicy.CamelCase);
+
+Console.WriteLine(typeof(JsonSerializerOptions).GetRuntimeFields());
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
